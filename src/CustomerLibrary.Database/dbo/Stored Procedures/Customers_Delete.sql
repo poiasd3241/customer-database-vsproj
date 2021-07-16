87d98034a7c8c0860b625b50557d10f7f2979831
Arseny Ivasyuk <poiasd3241@gmail.com>
2021-07-16 12:40:41 +0300

    Remove ON DELETE CASCADE for Notes

--- src/CustomerLibrary.Database/dbo/Stored Procedures/Customers_Delete.sql
+++ src/CustomerLibrary.Database/dbo/Stored Procedures/Customers_Delete.sql
@@ -5,6 +5,10 @@ BEGIN
     -- FK
     DELETE FROM [dbo].[Addresses]
 	WHERE [CustomerId] = @CustomerId
+	
+    -- FK
+	DELETE FROM [dbo].[Notes]
+	WHERE [CustomerId] = @CustomerId
 
     DELETE FROM [dbo].[Customers]
     WHERE [CustomerId] = @CustomerId
--- src/CustomerLibrary.Database/dbo/Tables/Notes.sql
+++ src/CustomerLibrary.Database/dbo/Tables/Notes.sql
@@ -3,5 +3,5 @@
     [Content]    NVARCHAR (1000) NOT NULL,
     [CustomerId] INT             NOT NULL,
     CONSTRAINT [PK_Notes_NoteId] PRIMARY KEY CLUSTERED ([NoteId] ASC),
-    CONSTRAINT [FK_Notes_Customers_CustomerId] FOREIGN KEY ([CustomerId]) REFERENCES [dbo].[Customers] ([CustomerId]) ON DELETE CASCADE
+    CONSTRAINT [FK_Notes_Customers_CustomerId] FOREIGN KEY ([CustomerId]) REFERENCES [dbo].[Customers] ([CustomerId])
 );
